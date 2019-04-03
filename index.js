9e09f16cbf8fecbd9faf0312a1795c208b5c7b6b
tetsu5555 <tetete1118@gmail.com>
2019-04-04 00:50:49 +0900

    Improve

--- index.js
+++ index.js
@@ -40,28 +40,25 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
-        let userName = ''
         axios.get(`/profile/${event.source.userId}`)
             .then(function (response) {
                 console.log(response)
-                userName = response.diplayName
+
+                // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+                if (event.type == "message" && event.message.type == "text") {
+                    // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+                    if (event.message.text == "こんにちは") {
+                        // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                        events_processed.push(bot.replyMessage(event.replyToken, {
+                            type: "text",
+                            text: `これはこれは、${response.diplayName}様`
+                        }));
+                    }
+                }
             })
             .catch(function (error) {
                 console.log('ERROR!! occurred in Backend.')
             });
-
-
-        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text") {
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは") {
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: `これはこれは、${userName}様`
-                }));
-            }
-        }
     });
 
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
