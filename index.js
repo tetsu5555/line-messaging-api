65979e5b78017aed03a139b6e7d2d937d36ac9f3
tetsu5555 <tetete1118@gmail.com>
2019-04-04 01:20:53 +0900

    Improve

--- index.js
+++ index.js
@@ -40,46 +40,46 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     let userIds = [];
 
     const reply = [
-        {
-            "type": "sticker",
-            "packageId": "1",
-            "stickerId": "1"
-        }
         // {
-        //     "type": "template",
-        //     "altText": "This is a buttons template",
-        //     "template": {
-        //         "type": "buttons",
-        //         "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-        //         "imageAspectRatio": "rectangle",
-        //         "imageSize": "cover",
-        //         "imageBackgroundColor": "#FFFFFF",
-        //         "title": "Menu",
-        //         "text": "Please select",
-        //         "defaultAction": {
-        //             "type": "uri",
-        //             "label": "View detail",
-        //             "uri": "http://example.com/page/123"
-        //         },
-        //         "actions": [
-        //             {
-        //                 "type": "postback",
-        //                 "label": "Buy",
-        //                 "data": "action=buy&itemid=123"
-        //             },
-        //             {
-        //                 "type": "postback",
-        //                 "label": "Add to cart",
-        //                 "data": "action=add&itemid=123"
-        //             },
-        //             {
-        //                 "type": "uri",
-        //                 "label": "View detail",
-        //                 "uri": "http://example.com/page/123"
-        //             }
-        //         ]
-        //     }
-        // },
+        //     "type": "sticker",
+        //     "packageId": "1",
+        //     "stickerId": "1"
+        // }
+        {
+            "type": "template",
+            "altText": "This is a buttons template",
+            "template": {
+                "type": "buttons",
+                "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover",
+                "imageBackgroundColor": "#FFFFFF",
+                "title": "Menu",
+                "text": "Please select",
+                "defaultAction": {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/123"
+                },
+                "actions": [
+                    {
+                        "type": "postback",
+                        "label": "Buy",
+                        "data": "action=buy&itemid=123"
+                    },
+                    {
+                        "type": "postback",
+                        "label": "Add to cart",
+                        "data": "action=add&itemid=123"
+                    },
+                    {
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "http://example.com/page/123"
+                    }
+                ]
+            }
+        }
         // {
         //     "type": "template",
         //     "altText": "this is a confirm template",
