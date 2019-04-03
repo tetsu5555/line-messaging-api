e1a998be01740efaff0f48a55fe1f627efb4be60
tetsu5555 <tetete1118@gmail.com>
2019-04-03 12:28:17 +0900

    Improve

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     console.log("++++++++++++++++++++++++++")
     console.log("++++++++++++++++++++++++++")
     // 先行してLINE側にステータスコード200でレスポンスする。
-    res.sendStatus(200);
+    // res.sendStatus(200);
 
     // すべてのイベント処理のプロミスを格納する配列。
     let events_processed = [];
